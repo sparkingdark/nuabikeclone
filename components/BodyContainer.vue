252543afef4b5fe1c6a623bd081902e87b18fe53
sparkingdark <debomastet335@gmail.com>
2022-01-12 22:24:20 +0530

    initial commit

---
+++ components/BodyContainer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="body">
+      <h1>hello</h1>      
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+</style>
---
+++ components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+<div class="navbar">
+    <div class="Bikes">
+    <a href="#">Bikes</a>
+    </div>
+    <div class="frames">
+    <a href="#">Frames</a>
+    </div>
+    <div>
+    <a data-testid="linkElement" data-anchor="dataItem-k8dcmg8x" href="https://www.nuabikes.com" target="_self" class="xQ_iF"><wix-image id="img_comp-k8a64np7" class="_1-6YJ _1Fe8-" data-image-info="{&quot;containerId&quot;:&quot;comp-k8a64np7&quot;,&quot;displayMode&quot;:&quot;fill&quot;,&quot;imageData&quot;:{&quot;width&quot;:1800,&quot;height&quot;:1580,&quot;uri&quot;:&quot;670ab1_f50b48a6843a4ff78aa90ac68e96b7ed~mv2.jpg&quot;,&quot;name&quot;:&quot;Nua%20logo%20con%20aire_edited.jpg&quot;,&quot;displayMode&quot;:&quot;fill&quot;,&quot;crop&quot;:{&quot;x&quot;:118,&quot;y&quot;:0,&quot;width&quot;:1539,&quot;height&quot;:1533,&quot;svgId&quot;:&quot;909695c1e003409ba70b5561666c7c4d.svg&quot;}}}" data-bg-effect-name="" data-is-svg="false" data-is-svg-mask="false" data-image-zoomed="" data-has-ssr-src="" data-src="https://static.wixstatic.com/media/670ab1_f50b48a6843a4ff78aa90ac68e96b7ed~mv2.jpg/v1/crop/x_118,y_0,w_1539,h_1533/fill/w_84,h_84,al_c,q_80,usm_0.66_1.00_0.01/Nua%2520logo%2520con%2520aire_edited.webp"><img src="https://static.wixstatic.com/media/670ab1_f50b48a6843a4ff78aa90ac68e96b7ed~mv2.jpg/v1/crop/x_118,y_0,w_1539,h_1533/fill/w_84,h_84,al_c,q_80,usm_0.66_1.00_0.01/Nua%2520logo%2520con%2520aire_edited.webp" alt="Nua%20logo%20con%20aire_edited.jpg" style="width:84px;height:84px;object-fit:cover;object-position:50% 50%"></wix-image></a>
+    </div>
+    <div class="gallery">
+    <a href="#">Gallery</a>
+    </div>
+    <div class="blog"> 
+    <a href="#">Blog</a>
+    </div>
+</div>
+</div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+.navbar{
+    background:transparent;
+    left:0;
+    width: 100%;
+    min-width: 980px;
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 20px;
+    
+}
+
+.blog{
+    position: relative;
+    right: 40px;
+
+}
+
+
+    
+
+</style>
+
+<script>
+</script>
