e274dbfe39e27a5d5d0e862b536f84110586a623
sparkingdark <debomastet335@gmail.com>
2022-01-16 21:19:48 +0530

    some text added

--- components/BodyContainer.vue
+++ components/BodyContainer.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="body">
-      <h1>hello</h1>      
+      <div class="gallery_photo">
+          <img src="https://static.wixstatic.com/media/670ab1_80f31deab79d4569a911fb8e59d5c337~mv2.jpg/v1/fill/w_700,h_372,al_c,q_80,usm_0.66_1.00_0.01/670ab1_80f31deab79d4569a911fb8e59d5c337~mv2.webp" alt="">
+      </div>
+      <div class="writings">
+          <h4>BESPOKE TITANIUM BIKES</h4>
+          <p>If you are looking for a titanium bicycle that’s custom made for  your needs, versatile, hi-tech, and long-lasting, you’ve come to the right place.
+             Welcome to Nua Bikes!Test</p>
+      </div>
     </div>
 </template>
 
@@ -9,4 +16,110 @@
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Barlow:wght@300;500&family=Open+Sans:wght@300&display=swap');
+.body{
+  display: flex;
+  height: 100vh;
+  margin: auto;
+
+}
+
+.gallery_photo{
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  position: relative;
+  padding: 80px 30px ;
+}
+
+.writings{
+  display: flexbox;
+  width: 300px;
+  justify-content: space-between;
+  align-self: start;
+  position: relative;
+  padding: 150px;
+  margin: 100px;
+  font: 'Barlow', sans-serif;;
+      --cookie-banner-primary-color: #fff;
+    --cookie-banner-secondary-color: #000;
+    --cookie-banner-font-family: HelveticaNeue,Helvetica Neue,helvetica,Sans-Serif;
+    --cookie-banner-font-size: 14px;
+    --color_0: 255,255,255;
+    --color_27: 186,131,240;
+    --color_1: 255,255,255;
+    --color_2: 0,0,0;
+    --color_3: 237,28,36;
+    --color_4: 0,136,203;
+    --color_5: 255,203,5;
+    --color_6: 114,114,114;
+    --color_7: 176,176,176;
+    --color_8: 255,255,255;
+    --color_28: 128,21,232;
+    --color_9: 114,114,114;
+    --color_10: 176,176,176;
+    --color_11: 255,255,255;
+    --color_12: 232,230,230;
+    --color_13: 199,199,199;
+    --color_14: 153,153,151;
+    --color_15: 0,0,0;
+    --color_16: 181,188,240;
+    --color_17: 143,152,226;
+    --color_18: 56,74,211;
+    --color_29: 85,14,155;
+    --color_19: 37,49,141;
+    --color_20: 19,25,70;
+    --color_21: 249,197,180;
+    --color_22: 243,167,143;
+    --color_23: 237,88,41;
+    --color_24: 158,59,27;
+    --color_25: 79,29,14;
+    --color_30: 43,7,77;
+    --color_31: 177,211,187;
+    --color_32: 127,168,139;
+    --color_33: 64,124,81;
+    --color_34: 43,83,54;
+    --color_35: 21,41,27;
+    --font_0: normal normal normal 40px/1.4em proxima-n-w01-reg,proxima-n-w05-reg,sans-serif;
+    --font_1: normal normal normal 16px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif;
+    --font_2: normal normal normal 20px/1.4em barlow-extralight,barlow,sans-serif;
+    --font_3: normal normal normal 60px/1.4em proxima-n-w01-reg,proxima-n-w05-reg,sans-serif;
+    --font_4: normal normal normal 40px/1.4em proxima-n-w01-reg,proxima-n-w05-reg,sans-serif;
+    --font_5: normal normal normal 25px/1.4em proxima-n-w01-reg,proxima-n-w05-reg,sans-serif;
+    --font_6: normal normal normal 22px/1.4em proxima-n-w01-reg,proxima-n-w05-reg,sans-serif;
+    --font_7: normal normal normal 17px/1.4em proxima-n-w01-reg,proxima-n-w05-reg,sans-serif;
+    --font_8: normal normal normal 15px/1.4em proxima-n-w01-reg,proxima-n-w05-reg,sans-serif;
+    --font_9: normal normal normal 14px/1.4em proxima-n-w01-reg,proxima-n-w05-reg,sans-serif;
+    --font_10: normal normal normal 12px/1.4em din-next-w01-light,din-next-w02-light,din-next-w10-light,sans-serif;
+    --wix-ads-height: 0px;
+    --wix-ads-top-height: 0px;
+    --site-width: 980px;
+    --above-all-z-index: 100000;
+    --portals-z-index: 100001;
+    --minViewportSize: 320;
+    --color_26: 210,172,247;
+    --maxViewportSize: 1920;
+    --pinned-layers-in-page: 0;
+    --pinned-layer-in-container: 51;
+    --above-all-in-container: 49;
+    --transition-duration: 0ms;
+    overflow-wrap: break-word;
+    text-align: start;
+    --min-height: 90px;
+    pointer-events: auto;
+    font: var(--font_2);
+    line-height: 1.2em;
+    letter-spacing: 0em;
+    font-family: barlow-extralight,barlow,sans-serif;
+    color: #241010;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    vertical-align: baseline;
+    background: transparent;
+    font-size: 24px;
+}
+
+
 </style>
